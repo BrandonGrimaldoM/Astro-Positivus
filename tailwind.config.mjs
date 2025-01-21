ff39a9a915d0f0cc4ef8c119ac70f79ad65665c6
Brandon James Grimaldo Moscote <jawesonline5@gmail.com>
2025-01-20 19:05:25 -0600

    50%

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,21 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors:{
+				"p-green": "#B9FF66",
+				"p-black": "#191A23",
+				"p-white": "#F3F3F3"
+			},
+			borderRadius:{
+				"big": "45px",
+				"little": "14px",
+				"mini": "7px"
+			},
+			boxShadow:{
+				"box": "0px 5px 0px 0px #191A23"
+			}
+		},
 	},
 	plugins: [],
 }
